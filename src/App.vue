691367833ba219924288c0302147201f4e6850d7
abhi92001 <abhishek942001@gmail.com>
2021-09-01 12:08:57 +0530

    Initialize project using Create React App

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav"></div>
+		<router-view />
+	</div>
 </template>
 
 <style>
-
-body{
-  
-  height: 100%;
-  margin:0px;
+body {
+	height: 500%;
+	margin: 0px;
+	width: 100%;
 }
 
-html{
-  background-color: #f9f7f7;
-  height: 100%;
-    max-width: 1440px;
-  margin: 0px auto;
+html {
+	background-color: #f9f7f7;
+	height: 100%;
+	/* max-width: 1440px; */
+	margin: 0px auto;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  height: 100%;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	height: 100%;
+	width: 100%;
 }
 </style>
